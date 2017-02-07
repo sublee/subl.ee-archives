36a59a1bb161f22cc0db3d51607aeabee3cea0f4
Heungsub Lee <sub@subl.ee>
2017-02-08 03:25:08 +0900

    Implement index.html builder

--- archives.py
+++ archives.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
+from datetime import date
 import os
 import re
+import subprocess
 
 import click
 
 
-@click.command()
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
 @click.argument('dest', type=click.Path(file_okay=False, exists=True))
 def preserve(dest):
     for dirname, __, filenames in os.walk(dest):
@@ -25,5 +32,29 @@ def preserve(dest):
                 f.write(data)
 
 
+@cli.command()
+@click.argument('src_repo', type=click.Path(file_okay=False, exists=True))
+def index(src_repo):
+    repo = os.path.dirname(os.path.abspath(__file__))
+    commits = []
+    for filename in os.listdir(repo):
+        if not os.path.isdir(filename):
+            continue
+        elif len(filename) != 7:
+            continue
+        commit = filename
+        time = int(subprocess.check_output([
+            'git', '-C', src_repo, '--no-pager',
+            'show', '-s', '--date=unix', '--format=%ad', commit,
+        ]))
+        commits.append((time, commit))
+    commits.sort(reverse=True)
+    print('<ul>')
+    for time, commit in commits:
+        print('  <li><a href="{commit}">{commit}</a> ({date:%Y-%m-%d})</li>'
+              ''.format(date=date.fromtimestamp(time), commit=commit))
+    print('</ul>')
+
+
 if __name__ == '__main__':
-    preserve()
+    cli()
